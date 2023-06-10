6fd7495773e696d63300aa5b538e9a151f5d9d92
joabeArthur <131968361+joabeArthur@users.noreply.github.com>
2023-06-10 19:36:43 -0300

    2

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function clickBotao() {
+    alert("comi o cu de quem leu")
+    let n1 = "1";
+    let n2 = "1";
+    let n3 = n1+n2;
+    console.log(n3)
+
+    let i_n1 = document.getElementById('n1')
+    let i_n2 = document.getElementById('n2')
+
+    console.log(Number(i_n1.value) + Number(i_n2.value));
+    
+}
